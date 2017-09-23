68697979f89568777fcffda69d5b86dc4989b338
Saud13 <alhosan.saud@gmail.com>
2017-09-23 01:56:00 -0400

    s

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "Hello World Test!";
-        ?>
-    </body>
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
 </html>
--- info.php
+++ info.php
@@ -1,8 +1,21 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+Password: <input type="text" name="Password"><br>
+<input type="submit">
+</form>
+
+</body>
+
+
+<?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+?>
 
